b3cd1e1c3118695d47804c82de020ec22a6a9889
David C. Campbell <david@dccampbell.com>
2019-05-12 20:57:47 -0400

    Add/improve customer tests

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -4,15 +4,15 @@ namespace Tests\Feature;
 
 use App\Customer;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class CustomerTest extends TestCase
 {
-    use WithFaker, RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     /** @test */
-    public function user_can_create_customer(): void
+    public function canCreateCustomer(): void
     {
         // create a unique email to test with
         $email = 'test+' . time() . '@example.com';
@@ -21,7 +21,7 @@ class CustomerTest extends TestCase
         /** @var Customer $customer */
         $customer = factory(Customer::class)->make(['email' => $email]);
 
-        // send a post request customer properties to the create endpoint
+        // send a request with the customer properties to the create endpoint
         $this->post('/customer', $customer->toArray());
 
         // confirm the customer was created in the database
@@ -29,5 +29,51 @@ class CustomerTest extends TestCase
 
         // confirm the unique email is now visible on the list page
         $this->get('/customer')->assertSee($email);
+    }
+
+    /** @test */
+    public function canUpdateCustomer(): void
+    {
+        // create a unique email to test with
+        $email = 'test+' . time() . '@example.com';
+
+        // generate and store a customer with the email
+        /** @var Customer $customer */
+        $customer = factory(Customer::class)->create(['email' => $email]);
+
+        // confirm the customer was created in the database
+        $this->assertDatabaseHas('customers', ['email' => $email]);
+
+        // create a new name for the user
+        $customer->name = 'Test Name';
+
+        // send a request with the updated name to the customer update endpoint
+        $this->put('/customer/' . $customer->id, $customer->toArray());
+
+        // confirm the customer name was updated in the database
+        $this->assertDatabaseHas('customers', ['email' => $email, 'name' => $customer->name]);
+    }
+
+    /** @test */
+    public function canDeleteCustomer(): void
+    {
+        // create a unique email to test with
+        $email = 'test+' . time() . '@example.com';
+
+        // generate and store a customer with the email
+        /** @var Customer $customer */
+        $customer = factory(Customer::class)->create(['email' => $email]);
+
+        // confirm the customer was created in the database
+        $this->assertDatabaseHas('customers', ['email' => $email]);
+
+        // send a request to the customer delete endpoint
+        $this->delete('/customer/' . $customer->id);
+
+        // confirm the customer was deleted from the database
+        $this->assertDatabaseMissing('customers', ['email' => $email]);
+
+        // confirm the unique email is not visible on the list page
+        $this->get('/customer')->assertDontSee($email);
     }
 }
